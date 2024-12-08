d34b0a2c5d7333bd349055d5768b1c04b3f728d8
JillianW10 <145229566+JillianW10@users.noreply.github.com>
2024-12-08 14:52:25 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,31 @@
 // JavaScript Document
 console.log("hi");
 
-let menuButton = document.querySelector("header section > button");
+const menuButton = document.querySelector("header section > button");
 const video = document.querySelector("video");
 const pauseButton = document.querySelector("#pauseButton");
-let sluitButton = document.querySelector("header nav > button");
+const sluitButton = document.querySelector("header nav > button");
 
 menuButton.addEventListener("click", openMenu);
 sluitButton.addEventListener("click", sluitMenu);
 pauseButton.addEventListener("click", pause);
-
-function openMenu(){
+ 
+function openMenu() {
     let nav = document.querySelector("nav");
     nav.classList.add("toonMenu");
-  }
-  
-function sluitMenu(){
+}
+
+function sluitMenu() {
     let nav = document.querySelector("nav");
     nav.classList.remove("toonMenu");
 }
-  
+
 function pause() {
     if (video.paused) {
-      video.play(); 
-      pauseButton.textContent = "Pause Video"; 
+        video.play();
+        pauseButton.textContent = "Pause Video";
     } else {
-      video.pause();
-      pauseButton.textContent = "Play Video"; 
+        video.pause();
+        pauseButton.textContent = "Play Video";
     }
 }
